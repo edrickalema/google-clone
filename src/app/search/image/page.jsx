6024a248f13c8a54319ce885bc10cc3c73118cb7
Alema Edrick <alemaedrick85@gmail.com>
2024-05-19 02:26:29 +0300

    created page for the search component

---
+++ src/app/search/image/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ImagePage() {
+  return <div>Image page</div>;
+}
+
+export default ImagePage;
---
+++ src/app/search/web/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function WebPage() {
+  return <div>web page page</div>;
+}
+
+export default WebPage;
